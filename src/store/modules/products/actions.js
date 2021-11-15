e12de1f8c2cecc4b6ebe7fa221e516542e5c9383
shubooo <shubooff@gmail.com>
2021-11-16 01:45:56 +0400

    eqweqw

--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -3,20 +3,20 @@ import axios from "axios";
 export default {
   async getProducts({ commit }) {
     const { data } = await axios.get(
-      "http://d04e-212-58-120-36.ngrok.io/products"
+      "https://d04e-212-58-120-36.ngrok.io/products"
     );
     commit("SET_PRODUCTS", data);
   },
 
   async deleteProducts({ state }) {
     const { data } = await axios.post(
-      "http://d04e-212-58-120-36.ngrok.io/delete",
+      "https://d04e-212-58-120-36.ngrok.io/delete",
       state.dProducts
     );
     return data;
   },
   addProduct({ state }) {
-    const data = fetch("http://d04e-212-58-120-36.ngrok.io/add", {
+    const data = fetch("https://d04e-212-58-120-36.ngrok.io/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
