7e4904eb9a8c211d7b614dc3b8bfe0223eec1650
shubooo <shubooff@gmail.com>
2021-11-15 23:45:49 +0400

    qwe

--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -3,14 +3,14 @@ import axios from "axios";
 export default {
   async getProducts({ commit }) {
     const { data } = await axios.get(
-      "https://scanditest2021.000webhostapp.com/products"
+      "https://scandiwebtest2022.000webhostapp.com/index.php/products"
     );
     commit("SET_PRODUCTS", data);
   },
 
   async deleteProducts({ state }) {
     const { data } = await axios.post(
-      "https://scanditest2021.000webhostapp.com/delete",
+      "https://scandiwebtest2022.000webhostapp.com/index.php/delete",
       state.dProducts
     );
     return data;
@@ -22,7 +22,7 @@ export default {
       body: JSON.stringify({ title: state.addProduct }),
     };
     const response = await fetch(
-      "https://scanditest2021.000webhostapp.com/add",
+      "https://scandiwebtest2022.000webhostapp.com/index.php/add",
       requestOptions
     );
     const data = await response.json();
