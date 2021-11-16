24560d2659d31e8afcb2534401d105cb408329d1
shubooo <shubooff@gmail.com>
2021-11-16 04:30:18 +0400

    wqeqwe

--- src/store/modules/products/actions.js
+++ src/store/modules/products/actions.js
@@ -3,26 +3,29 @@ import axios from "axios";
 export default {
   async getProducts({ commit }) {
     const { data } = await axios.get(
-      "https://scandiwebtest2022.000webhostapp.com/products"
+      "https://scandiwebtest2022.000webhostapp.com/index.php/products"
     );
     commit("SET_PRODUCTS", data);
   },
 
   async deleteProducts({ state }) {
     const { data } = await axios.post(
-      "https://scandiwebtest2022.000webhostapp.com/delete",
+      "https://scandiwebtest2022.000webhostapp.com/index.php/delete",
       state.dProducts
     );
     return data;
   },
   addProduct({ state }) {
-    const data = fetch("https://scandiwebtest2022.000webhostapp.com/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(state.addProduct),
-    })
+    const data = fetch(
+      "https://scandiwebtest2022.000webhostapp.com/index.php/add",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state.addProduct),
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         return data;
